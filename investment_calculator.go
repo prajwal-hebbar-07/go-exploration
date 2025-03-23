4b5fe6ebb8690c3c704b275cd3f336452ef7453b
Hebbar <prajwalhebbaras@gmail.com>
2025-03-23 10:09:26 +0530

    shorthand notation to declare all variables

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,8 +6,7 @@ import (
 )
 
 func main() {
-	var investmentAmount, years float64 = 1000, 10
-	expectedReturns := 5.5
+	investmentAmount, years, expectedReturns := 1000.0, 10.0, 5.5
 
 	futureValue := investmentAmount + math.Pow(1+expectedReturns/100, years)
 	fmt.Println(futureValue)
