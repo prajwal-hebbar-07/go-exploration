5847647fd65eb54a2fdd1f4f609c1bb5c70f2676
Hebbar <prajwalhebbaras@gmail.com>
2025-03-24 14:42:52 +0530

    add inflation into the equation to fetch the real value

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 	// investmentAmount, years, expectedReturns := 1000.0, 10.0, 5.5
+	const inflationRate float64 = 2.5
 	var investmentAmount float64 = 1000
 	var years float64 = 10
 	var expectedReturns float64 = 5.5
 
 	var futureValue float64 = investmentAmount + math.Pow(1+expectedReturns/100, years)
+	var futureRealValue float64 = futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
