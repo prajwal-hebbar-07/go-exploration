4bd71be1d6d087b0d97b46a1a8446a63f2c3c1a7
Hebbar <prajwalhebbaras@gmail.com>
2025-03-24 15:32:51 +0530

    style: format future value output to two decimal places
    Improve readability by formatting the future value and future real value output to two decimal places for better clarity and precision

--- investment_calculator.go
+++ investment_calculator.go
@@ -25,5 +25,6 @@ func main() {
 
 	// fmt.Println("Future Value: ", futureValue)
 	// fmt.Println("Future Real Value adjusted for Inflation: ", futureRealValue)
-	fmt.Printf("Future Value: %v\nFuture Value adjusted for Inflation: %v\n", futureValue, futureRealValue)
+	// fmt.Printf("Future Value: %v\nFuture Value adjusted for Inflation: %v\n", futureValue, futureRealValue)
+	fmt.Printf("Future Value: %.2f\nFuture Value adjusted for Inflation: %.2f\n", futureValue, futureRealValue)
 }
